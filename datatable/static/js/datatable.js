c2506bb0b0a4885c7fdd903bd7e894524c74eba4
Thomas Simeão <simeaothomas@gmail.com>
2022-05-05 09:22:53 -0300

    Tabela completa sem server-side datatable.

--- datatable/static/js/datatable.js
+++ datatable/static/js/datatable.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $('#example').DataTable({
         "processing": true,
-        "serverSide": true,
+        // "serverSide": true,
+
         "ajax": "api/"
     });
 });
